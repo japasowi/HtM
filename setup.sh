204d26ac539b7d8ad698be44b5ac2c7cb60b5305
Nathaniel Priddy <ngpriddy@gmail.com>
2019-08-29 11:29:23 -0400

    Adding Docker CE Setup

--- setup.sh
+++ setup.sh
@@ -5,3 +5,9 @@ apt-get upgrade
 firewall-cmd --remove-service=ssh --permanent
 firewall-cmd --reload
 passwd -l root
+apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
